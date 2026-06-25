e2288dda68679e4a7d462ddf01a28443883bead1
Hirushi Karunasingha <hirushikarunasingha6@gmail.com>
2026-06-25 06:17:34 +0530

    Update Password_v1.py

--- Password-Srength-Checker/Password_v1.py
+++ Password-Srength-Checker/Password_v1.py
@@ -1,1 +1,9 @@
 
+password=input("Enter your password: ")
+print("your password: ",password) 
+common_passwords=(1234,123456,"password","user")
+
+if any(c.isupper() for c in password)==False:
+    print("create a password including capital letter for more secure")
+elif any(c.islower() for c in password)==False:
+    print("create a password including simple letter for more secure")
